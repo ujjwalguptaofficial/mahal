877f74013300ec8301c792e42b64828cca6f77b7
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-02-28 18:20:30 +0530

    adjust installer with respect to folder change

--- tests/installer.js
+++ tests/installer.js
@@ -2,11 +2,11 @@ const { readFileSync } = require('fs');
 const { execSync } = require('child_process');
 const path = require('path');
 
-const content = readFileSync("../../package.json");
+const content = readFileSync("../package.json");
 
 const packageInfo = JSON.parse(content);
 
-const compilerFolderLocation = path.join(__dirname, "../../../mahal-html-compiler");
+const compilerFolderLocation = path.join(__dirname, "../../mahal-html-compiler");
 console.log("folderLocation", compilerFolderLocation);
 const compilerContent = readFileSync(`${compilerFolderLocation}/package.json`);
 const compilerPackageInfo = JSON.parse(compilerContent);
@@ -14,9 +14,9 @@ const compilerPackageInfo = JSON.parse(compilerContent);
 if (packageInfo) {
     const version = packageInfo.version;
     console.log('version', version);
-    execSync(`npm i ../../mahal-${version}.tgz`);
+    execSync(`npm i ../mahal-${version}.tgz`);
     execSync(`npm i ${compilerFolderLocation}/mahal-html-compiler-${compilerPackageInfo.version}.tgz`);
-    execSync(`npm i ../../../mahal-test-utils/mahal-test-utils-0.1.0.tgz --no-save`);
+    execSync(`npm i ../../mahal-test-utils/mahal-test-utils-0.1.0.tgz --no-save`);
 }
 else {
     throw "no package found";
