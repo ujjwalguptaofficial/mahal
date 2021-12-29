97a4029a184eb671ee1e1a06aa5b55aeef9ec25b
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2021-12-29 16:37:49 +0530

    optimize class directive

--- src/ready_made/add_class_directive.ts
+++ src/ready_made/add_class_directive.ts
@@ -2,38 +2,31 @@ import { IDirectiveBinding } from "../interface";
 import { isObject } from "../utils";
 
 export const classDirective = (el: HTMLElement, binding: IDirectiveBinding) => {
-    const isValueObject = isObject(binding.value[0]);
 
-    const addClass = () => {
-        if (binding.params.length > 1) {
-            binding.value.forEach(value => {
-                el.classList.add(value);
-            });
-        }
-        else {
-            const classValue = binding.value[0];
-            if (isValueObject) {
-                const classes = classValue;
-                for (const name in classes) {
-                    if (classes[name]) {
-                        el.classList.add(name);
-                    }
+    const applyClass = (classes) => {
+        if (isObject(classes)) {
+            for (const name in classes) {
+                if (classes[name]) {
+                    applyClass(name);
+                }
+                else {
+                    el.classList.remove(name);
                 }
             }
-            else {
-                el.className += ` ${classValue}`;
-            }
         }
+        else {
+            el.classList.add(classes);
+        }
+    }
+
+    const addClass = () => {
+        binding.value.forEach(value => {
+            applyClass(value);
+        });
     };
     addClass();
     return {
         valueUpdated() {
-            if (isValueObject) {
-                const classes = binding.value[0];
-                for (const name in classes) {
-                    el.classList.remove(name);
-                }
-            }
             addClass();
         }
     };
