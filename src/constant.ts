6bc503bcd44d12aa9e5249eef066342bd73d7469
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-02-18 20:42:50 +0530

    release v - 0.21.0

--- src/constant.ts
+++ src/constant.ts
@@ -1,19 +1,1 @@
-import { getFromWindow } from "./utils";
-
-// export const globalFormatter = {
-//     toS: function (value) {
-//         switch (typeof value) {
-//             case 'string':
-//                 return value;
-//             case 'number':
-//                 return (value as number).toString();
-//             default:
-//                 return JSON.stringify(value);
-//         }
-//     }
-// }
-// export const globalComponents = {}
-// export const globalDirectives = {}
-// export const plugins = [];
 export const defaultSlotName = "default"
-// export const replacedBy = "replacedBy"
--- src/helpers/handle_expression.ts
+++ src/helpers/handle_expression.ts
@@ -9,46 +9,43 @@ export function handleExpression(this: Component, method: () => Promise<HTMLElem
     if (type === "for") {
         return handleForExp.call(this, keys[0], method);
     }
-    return new Promise((res) => {
-        method().then(el => {
-            const changesQueue = [];
-            const handleChange = () => {
-                changesQueue.shift();
-                const onChange = () => {
-                    nextTick(() => {
-                        method().then(newEl => {
-                            replaceEl(el, newEl);
-                            el = newEl;
-                            handleChange();
-                        }).catch(err => {
-                            emitError.call(this, err);
-                        });
+    return method().then(el => {
+        const changesQueue = [];
+        const handleChange = () => {
+            changesQueue.shift();
+            const onChange = () => {
+                nextTick(() => {
+                    method().then(newEl => {
+                        replaceEl(el, newEl);
+                        el = newEl;
+                        handleChange();
+                    }).catch(err => {
+                        emitError.call(this, err);
                     });
-                };
-                const watchCallBack = () => {
-                    changesQueue.push(1);
-                    if (changesQueue.length === 1) {
-                        onChange();
-                    }
-                };
-                keys.forEach(item => {
-                    this.watch(item, watchCallBack);
                 });
-                const onElDestroyed = function () {
-                    el.removeEventListener(LIFECYCLE_EVENT.Destroy, onElDestroyed);
-                    keys.forEach(item => {
-                        this.unwatch(item, watchCallBack);
-                    });
-                }.bind(this);
-                el.addEventListener(LIFECYCLE_EVENT.Destroy, onElDestroyed);
-                if (changesQueue.length > 0) {
+            };
+            const watchCallBack = () => {
+                changesQueue.push(1);
+                if (changesQueue.length === 1) {
                     onChange();
                 }
-                emitUpdate(this);
             };
-            handleChange();
-            res(el);
-        });
+            keys.forEach(item => {
+                this.watch(item, watchCallBack);
+            });
+            const onElDestroyed = function () {
+                el.removeEventListener(LIFECYCLE_EVENT.Destroy, onElDestroyed);
+                keys.forEach(item => {
+                    this.unwatch(item, watchCallBack);
+                });
+            }.bind(this);
+            el.addEventListener(LIFECYCLE_EVENT.Destroy, onElDestroyed);
+            if (changesQueue.length > 0) {
+                onChange();
+            }
+            emitUpdate(this);
+        };
+        handleChange();
+        return el;
     });
-
 }
