ae56f7b5093a0abfb651cc0e1c36c8835d856f73
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-03-04 20:41:44 +0530

    add same version install for related modules in prepublish

---
+++ tests/install_same_version.js
@@ -0,0 +1,31 @@
+const { readFileSync } = require('fs');
+const { execSync } = require('child_process');
+const path = require('path');
+
+const content = readFileSync("../package.json");
+
+const packageInfo = JSON.parse(content);
+
+// const compilerFolderLocation = path.join(__dirname, "../../mahal-html-compiler");
+// console.log("folderLocation", compilerFolderLocation);
+// const compilerContent = readFileSync(`${compilerFolderLocation}/package.json`);
+// const compilerPackageInfo = JSON.parse(compilerContent);
+
+// const testUtilsFolderLocation = path.join(__dirname, "../../mahal-test-utils");
+// console.log("testUtilsFolderLocation", testUtilsFolderLocation);
+// const testUtilsContent = readFileSync(`${testUtilsFolderLocation}/package.json`);
+// const testUtilsPackageInfo = JSON.parse(testUtilsContent);
+
+// execSync(`npm i ${compilerFolderLocation}/mahal-html-compiler-${compilerPackageInfo.version}.tgz`);
+// execSync(`npm i ../../mahal-test-utils/mahal-test-utils-${testUtilsPackageInfo.version}.tgz`);
+
+if (packageInfo) {
+    const version = packageInfo.version;
+    console.log('version', version);
+    execSync(`npm i mahal-test-utils@${version}`);
+    execSync(`npm i mahal-html-compiler@${version}`);
+
+}
+else {
+    throw "no package found";
+}
