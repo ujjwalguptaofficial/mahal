2ab5d682238f412b51b6f6f9eb27f0c45dde822d
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-07-02 17:11:05 +0530

    optimize reset

--- src/helpers/dispatch_destroy.ts
+++ src/helpers/dispatch_destroy.ts
@@ -1,12 +1,5 @@
 import { nextTick } from "../utils";
 
-
-
-// const dispatchDestroyedEv = (node: Node) => {
-
-
-// };
-
 export const dispatchDestroyed = (node: Node) => {
     node.childNodes.forEach(dispatchDestroyed);
     const destroyEvs = (node as any).__destroyev__;
