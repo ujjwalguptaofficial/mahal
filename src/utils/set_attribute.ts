a39db993151e7284d309af9c7d6809ac4e43f022
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-08-01 10:05:47 +0530

    optimize setAttribute method

--- src/utils/set_attribute.ts
+++ src/utils/set_attribute.ts
@@ -1,13 +1,16 @@
-import { KEY, MAHAL_KEY } from "../constant";
+import { MAHAL_KEY } from "../constant";
 
 export const setAttribute = (element: HTMLElement, key: string, value: string) => {
-    if (key === KEY) {
-        element[MAHAL_KEY] = value;
-        return;
+    switch (key) {
+        case 'key':
+            element[MAHAL_KEY] = value;
+            break;
+        case "value":
+            if (element.nodeType === 1) {
+                (element as HTMLInputElement).value = value;
+                break;
+            }
+        default:
+            element.setAttribute(key, value);
     }
-    // if (element.nodeType === 8) return;
-    if (element.nodeType === 1 && key === "value") {
-        return (element as HTMLInputElement).value = value;
-    }
-    return element.setAttribute(key, value);
 };
