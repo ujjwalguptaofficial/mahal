cfeb66c374ec51ab3575e1ae9eabd76b52a82c52
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-08-03 20:58:57 +0530

    remove errors which are not used in production

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -42,37 +42,46 @@ export class Logger implements IError {
 
     private getMsg_() {
         const info = this.info_;
-        switch (this.type) {
-            case ERROR_TYPE.PropDataTypeMismatch:
-                let str = `Expected Data type of property ${this.info_.prop} is ${this.info_.exp} but received ${this.info_.got}.${newLine}`;
-                if (this.info_.template) {
-                    str += `in template -${newLine}"${this.info_.html}"${newLine}`;
+        const prodErrors = {
+            [ERROR_TYPE.PropDataTypeMismatch]() {
+                let str = `Expected Data type of property ${info.prop} is ${info.exp} but received ${info.got}.${newLine}`;
+                if (info.template) {
+                    str += `in template -${newLine}"${info.html}"${newLine}`;
                 }
-                if (this.info_.file) {
-                    str += `in file - ${this.info_.file} `;
+                if (info.file) {
+                    str += `in file - ${info.file} `;
                 }
                 return str;
-            case ERROR_TYPE.SynTaxError:
-                return this.info_;
-            case ERROR_TYPE.ForExpAsRoot:
-                return `For is not allowed in root element. Create a child element instead.`;
-            case ERROR_TYPE.ForOnPrimitiveOrNull:
-                return `For expression can not be run on null or primitive datatype. Initiate variable ${this.info_} as array or object.`;
-            case ERROR_TYPE.InvalidEventHandler:
-                return `Invalid event handler for event "${this.info_.ev}", Handler does not exist in component.`;
-            case ERROR_TYPE.InvalidComponent:
-                return `Component "${this.info_.tag}" is not registered. Make sure you have registered component either in parent component or globally.`;
-            case ERROR_TYPE.InvalidFormatter:
-                return `Can not find Formatter "${this.info_.formatter}". Make sure you have registered formatter either in component or globally.`;
-            case ERROR_TYPE.MutatingProp:
-                return `Do not mutate prop "${this.info_.key}" directly. Instead use a reactive property.`
-                    + newLine + `found in -` + newLine +
-                    `${this.info_.html}`;
-            case ERROR_TYPE.SetSameValue:
-                return `Component won't be updated, because of old value and new value are same for state - ${info.key}.`;
-            default:
-                return this.msg;
+            },
+            [ERROR_TYPE.InvalidComponent]() {
+                return `Component "${info.tag}" is not registered. Make sure you have registered component either in parent component or globally.`;
+            },
+            [ERROR_TYPE.InvalidFormatter]() {
+                return `Can not find Formatter "${info.formatter}". Make sure you have registered formatter either in component or globally.`;
+            },
+        };
+
+        if (process.env.NODE_ENV !== 'production') {
+            const devErrors = {
+                [ERROR_TYPE.ForOnPrimitiveOrNull]() {
+                    return `For expression can not be run on null or primitive datatype. Initiate variable ${info} as array or object.`;
+                },
+                [ERROR_TYPE.InvalidEventHandler]() {
+                    return `Invalid event handler for event "${info.ev}", Handler does not exist in component.`;
+                },
+                [ERROR_TYPE.SetSameValue]() {
+                    return `Component won't be updated, because of old value and new value are same for state - ${info.key}.`;
+                },
+                [ERROR_TYPE.MutatingProp]() {
+                    return `Do not mutate prop "${info.key}" directly. Instead use a reactive property.`
+                        + newLine + `found in -` + newLine +
+                        `${info.html}`;
+                }
+            };
+            Object.assign(prodErrors, devErrors);
         }
+        const method = prodErrors[this.type];
+        return method ? method(this) : this.msg;
     }
 
     static warn(...args) {
@@ -80,9 +89,9 @@ export class Logger implements IError {
     }
 
     static error(...args) {
-        if (args[0] === "return") {
+        // if (args[0] === "return") {
 
-        }
+        // }
         console.error(`{${libName} error}:`, ...args);
     }
 
