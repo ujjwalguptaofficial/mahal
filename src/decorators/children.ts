6e2a241ad91d05888d8cbe2ed22ae7362bafb3ca
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-08-20 19:27:56 +0530

    rm naming of function in decorator - children and template

--- src/decorators/children.ts
+++ src/decorators/children.ts
@@ -4,7 +4,7 @@ import { ILazyComponent } from "../interface";
 // tslint:disable-next-line
 export const Children = (value: { [key: string]: typeof Component | Promise<any> | Function | ILazyComponent }) => {
     // tslint:disable-next-line
-    return function Template<T extends { new(...args: any[]): {} }>(constructor: T) {
+    return <T extends { new(...args: any[]): {} }>(constructor: T) => {
         return class extends constructor {
             children = value;
         };
